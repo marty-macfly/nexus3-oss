aa63f48cbb658265314734e2e5f54966b2428256
Charles DELFLY <c.delfly@linkbynet.com>
2018-12-06 12:59:59 +0700

    Avoid exception if repository doesn't exist

--- files/groovy/delete_repo.groovy
+++ files/groovy/delete_repo.groovy
@@ -1,5 +1,19 @@
 import groovy.json.JsonSlurper
 
-parsed_args = new JsonSlurper().parseText(args)
+def parsed_args = new JsonSlurper().parseText(args)
 
-repository.getRepositoryManager().delete(parsed_args.name)
+if(arg.name == null) {
+    return "you need to provid a repository 'name'"
+}
+
+Repository repo = repository.repositoryManager.get(arg.name)
+
+if (repo != null) {
+    try {
+        repository.repositoryManager.delete(repo.name)
+        repo.destroy()
+    } catch(Exception e) {
+        return e.toString()
+    }
+    return true
+}
