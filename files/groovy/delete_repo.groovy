40d72571723668df181127f597a1af51c9da515e
Charles DELFLY <c.delfly@linkbynet.com>
2018-12-06 13:03:00 +0700

    Improve delete repo grovy script to check repository and destroy it

--- files/groovy/delete_repo.groovy
+++ files/groovy/delete_repo.groovy
@@ -1,11 +1,23 @@
-import groovy.json.JsonSlurper
+/**
+ * Delete the repsotiroy of the name given in input if exist
+ *
+ * Input is json:
+ *   name: Repository name
+ * Output is json:
+ *   result: "true" if ok
+ *           "error message" if not ok
+ *           "null" if repository doesn't exist
+ *
+ **/
 
-def parsed_args = new JsonSlurper().parseText(args)
+import groovy.json.JsonSlurper
+import org.sonatype.nexus.repository.Repository
+
+def arg = new JsonSlurper().parseText(args)
 
 if(arg.name == null) {
     return "you need to provid a repository 'name'"
 }
-
 Repository repo = repository.repositoryManager.get(arg.name)
 
 if (repo != null) {
